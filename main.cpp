6d9cb5d26f7bccb314a8378fac70b6ca36f4ab4d
Gerard Garcia <g90294@gmail.com>
2021-03-15 12:42:46 -0600

    Creacion de funcion push 3.0

--- main.cpp
+++ main.cpp
@@ -1,2 +1,51 @@
 #include<iostream>
-#include<cnio.h>
+#include<conio.h>
+#define MAX 8
+
+void push(int [], int, int&, int);
+
+int main() {
+
+    int option, stack[MAX], top = -1;
+    char fact;
+
+    do{
+        cout << ">>>>  PILAS   <<<<" << endl << endl;
+        cout << "> Insertar Datos (1)" << endl;
+        cout << "> Eliminar Datos (2)" << endl;
+        cout << "> Cerrar Programa (3)" << endl;
+        cout << "> Seleccionar: "
+
+        cin >> option;
+        switch(option) {
+            case 1:
+                cout << "Que dato desea eliminar?"; cin >> fact;
+                push(stack, top, MAX, fact);
+            break;
+
+            case 2:
+
+            break;
+        }
+
+    } while (option != 3);
+
+
+    getch();
+    return 0;
+}
+
+//Funcion de ususario
+
+//INSERT
+void push(int stack[], int top, int& max , int fact) {
+    bool flag;
+    if (flag){
+        cout << "Overflow (Desbordamiento de la pila)";
+    } else {
+        top++;
+        stack[top] = fact;
+    }
+}
+
+//STATUS
