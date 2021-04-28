86ebacd1e0e43ef324cb4e8dda27b11962f3f2f0
Gerard Garcia <g90294@gmail.com>
2021-04-28 13:07:59 -0500

    Función eliminar añadida

--- ListaSimple.cpp
+++ ListaSimple.cpp
@@ -1,15 +1,16 @@
 // by Juan Gerardo Molina Garcia and Miguel Enrique Roque Ramos
 #include <iostream>
 #include <conio.h>
+#include <stdlib.h>
 
 struct Nodo {
     int data;
     Nodo *next;
 };
 
-void inputO(Nodo *&, int);
 void inputD(Nodo *&, int);
 void showList(Nodo *);
+void deleteElement(Nodo *&, int);
 
 int main() {
     Nodo *list = NULL;
@@ -19,10 +20,27 @@ int main() {
 	do {
 		cout << "Digite un numero:";
 		cin >> data;
-		inputO(list, data);
+		inputD(list, data);
 
 		cout << "\nDesea agregar un nuevo dato en lista (s/n)" << endl;
 		cin >> resp;
+	} while(resp == 's' || resp == 'S');
+
+    do {
+        if (list == NULL) {
+            cout << "Imposible realizar esta accion - Lista vacia";
+            cout << endl;
+            system("pause");
+            exit(-1);
+        } else {
+            cout << "Digite un numero a eliminar:";
+            cin >> data;
+            deleteElement(list, data);
+
+            cout << "\nDesea eliminar un nuevo dato en lista? (s/n)" << endl;
+            cin >> resp;
+        }
+
 	} while(resp == 's' || resp == 'S');
 
     getch();
@@ -51,33 +69,34 @@ void inputD(Nodo *&list, int data) {
     showList(list);
 }
 
-void inputO(Nodo *&list, int data) {
-    Nodo *new_nodo = new Nodo();
-    new_nodo->data = data;
-
-    Nodo *aux1 = list;
-    Nodo *aux2;
-
-    while ((aux1 != NULL) && (aux1->data < data)) {
-        aux2 = aux1;
-        aux1 = aux1->next;
-    }
-
-    if (list == aux1) {
-        list = new_nodo;
-    } else {
-        aux2->next = new_nodo;
-    }
-
-    new_nodo->next = aux1;
-    cout << "\n Elemento " << data << " insertado correctamente" << endl << endl;
-
-    showList(list);
-}
-
 void showList(Nodo *list) {
     while( !(list == NULL)) {
         cout << list->data << "->";
         list = list->next;
     }
+}
+
+void deleteElement(Nodo *&list, int data2) {
+    Nodo* aEliminar;
+    Nodo* aux = list;
+
+
+    while(aux != NULL) {
+        if (list->data == data2) {
+            aEliminar = list;
+            list = list->next;
+            aux = aux->next;
+            delete aEliminar;
+        } else {
+            if(aux->next != NULL && aux->next->data == data2) {
+                aEliminar = aux->next;
+                aux->next = aux->next->next;
+                delete aEliminar;
+            } else {
+                aux = aux->next;
+            }
+        }
+    }
+
+     showList(list);
 }
